53299f7a4fe133d63f2fc572c6ab1da8b1ddce49
Amit-Shaw7 <maahiamit777@gmail.com>
2023-09-04 02:01:54 +0530

    corrected url

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,8 +1,7 @@
-const frontentUrl = process.env.FRONTEND_URL;
 
 export const allowedOrigins = [
     "http://localhost:3000",
     "http://localhost:3030",
     "http://localhost:8000",
-    "https://managet.vercel.app/"
+    "https://managet.vercel.app"
 ]
