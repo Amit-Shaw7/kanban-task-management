b531fdda4cb84803c2f826ab44f9ffbe5c5f70fa
Amit-Shaw7 <maahiamit777@gmail.com>
2023-09-04 19:51:00 +0530

    fixed loader color in light and dark mode

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loader = () => {
     return (
-        <div className='h-[91vh] w-[100vw] flex items-center justify-center'>
+        <div className='bg-light dark:bg-dark h-[91vh] w-[100vw] flex items-center justify-center'>
             <span class="animate-ping absolute inline-flex h-10 w-10 rounded-full bg-btn opacity-75"></span>
         </div>
     )
